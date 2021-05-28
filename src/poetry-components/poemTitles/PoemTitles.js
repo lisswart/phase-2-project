ca522233960d7ad06443b686b45a24bf7726e970
lisswart <arnoudlisa@gmail.com>
2021-05-28 01:27:54 -0400

    .

--- src/poetry-components/poemTitles/PoemTitles.js
+++ src/poetry-components/poemTitles/PoemTitles.js
@@ -8,11 +8,11 @@ function PoemTitles({ titles, handleClick }) {
             <ol className="poem-titles">
                 {Array.from(titles).map((title, index) => (
                     <li className="poem-container" key={index}>
-                        <a href="javascript:void(0)" onClick={handleClick(title.title)} className="click-title">
+                        {/* <a href="javascript:void(0)" onClick={handleClick(title.title)} className="click-title"> */}
                             <h2 className="title">
                                 <i>{title.title}</i>
                             </h2>
-                        </a>
+                        {/* </a> */}
                         <h3 className="author">{title.author}</h3>
                         <p>linecount: {title.linecount}</p>                        
                     </li>
