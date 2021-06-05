f1134942cc62e76645ef10217fa7347589ca3415
lisswart <arnoudlisa@gmail.com>
2021-06-05 19:36:21 -0400

    added timed words

---
+++ src/DisplayHangmanWord.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function DisplayHangmanWord() {
+    const [words, setWords] = useState("drill");
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        fetch("https://hidden-harbor-11546.herokuapp.com/words")
+            .then(r => r.json())
+            .then(setWords);
+    }, [time]);
+
+    useEffect(() => {
+        const timerID = setInterval(() => {
+            setTime(new Date());
+        }, 18e3);
+        //return clearInterval(timerID);
+    }, []);
+
+    function selectWord() {
+        return words[Math.floor(Math.random() * words.length)];
+    }
+
+    return (
+        <div style={{color: "cornsilk", marginLeft: "2em"}}>
+            {selectWord()}
+        </div>
+    );
+}
+
+export default DisplayHangmanWord;
